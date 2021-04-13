1ac58b054d975d6b74bfa3b380281727529743dd
Generosa Litton <glcodeworks@gmail.com>
2021-04-13 16:59:33 -0700

    removed netlify, added wordpress

--- README.md
+++ README.md
@@ -9,19 +9,20 @@
 </h1>
 
 A (slightly) opinionated Gatsby Strapi boilerplate to get started fast. Stack includes:
-* [Bootstrap](https://getbootstrap.com/)
-* [Reactstrap](https://reactstrap.github.io/)
-* [Reactstrap-form-fields](https://github.com/jeremylynch/reactstrap-form-fields)
-* [Reactstrap-json-nav](https://github.com/jeremylynch/reactstrap-json-nav)
-* [React Icons](https://github.com/react-icons/react-icons)
-* [Google Tag Manager](https://github.com/gatsbyjs/gatsby/tree/master/packages/gatsby-plugin-google-tagmanager)
-* [gatsby-plugin-netlify](https://github.com/gatsbyjs/gatsby/tree/master/packages/gatsby-plugin-netlify)
-* [gatsby-plugin-google-tagmanager](https://www.gatsbyjs.org/packages/gatsby-plugin-google-tagmanager/)
-* [gatsby-plugin-facebook-pixel](https://www.gatsbyjs.org/packages/gatsby-plugin-facebook-pixel)
-* [gatsby-plugin-manifest](https://www.gatsbyjs.com/plugins/gatsby-plugin-manifest/)
-* [gatsby-plugin-styled-components](https://www.gatsbyjs.org/packages/gatsby-plugin-styled-components/)
 
-[View Demo](https://gatsby-strapi-starter.netlify.com/)
+- [Bootstrap](https://getbootstrap.com/)
+- [Reactstrap](https://reactstrap.github.io/)
+- [Reactstrap-form-fields](https://github.com/jeremylynch/reactstrap-form-fields)
+- [Reactstrap-json-nav](https://github.com/jeremylynch/reactstrap-json-nav)
+- [React Icons](https://github.com/react-icons/react-icons)
+- [Google Tag Manager](https://github.com/gatsbyjs/gatsby/tree/master/packages/gatsby-plugin-google-tagmanager)
+- [gatsby-plugin-google-tagmanager](https://www.gatsbyjs.org/packages/gatsby-plugin-google-tagmanager/)
+- [gatsby-plugin-facebook-pixel](https://www.gatsbyjs.org/packages/gatsby-plugin-facebook-pixel)
+- [gatsby-plugin-manifest](https://www.gatsbyjs.com/plugins/gatsby-plugin-manifest/)
+- [gatsby-plugin-styled-components](https://www.gatsbyjs.org/packages/gatsby-plugin-styled-components/)
+
+[View Original Demo](https://gatsby-strapi-starter.netlify.com/)
+[View Gatsby Cloud Demo](TO DO: ADD URL)
 
 ## 🚀 Quick start
 
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,32 @@
-let facebook_pixel = ''
-let gtm = '1234'
+let facebook_pixel = ""
+let gtm = "1234"
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Landing Page Starter`,
-    description: `A slightly opinionated Gatsby Landing Page starter template. `,
+    title: `Gatsby Landing Page Starter Modified for Wordpress`,
+    description: `A slightly opinionated Gatsby Landing Page starter template. Modified to work with Wordpress`,
     author: `Jeremy Lynch`,
-    siteUrl: `https://gatsby-strapi-starter.netlify.com/`,
-    phone: '1800123456',
-    fax: '180012345',
-    address: '123 fake street',
-    email: 'contact@test.com'
-
+    modified: `Generosa Litton for using with Wordpress`,
+    originalSiteUrl: `https://gatsby-strapi-starter.netlify.com/`,
+    gatsbyCloudUrl: ``,
+    phone: "1800123456",
+    fax: "180012345",
+    address: "123 fake street",
+    email: "contact@test.com",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        /*
+         * The full URL of the WordPress site's GraphQL API.
+         * Example : 'https://www.example-site.com/graphql'
+         */
+        url: `https://gatsbysales.wpengine.com/graphql`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -24,29 +35,28 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-google-tagmanager',
+      resolve: "gatsby-plugin-google-tagmanager",
       options: {
         id: gtm,
-        includeInDevelopment: false
-      }
+        includeInDevelopment: false,
+      },
     },
     {
       resolve: `gatsby-plugin-facebook-pixel`,
       options: {
         pixelId: facebook_pixel,
-      }
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        icon: './src/assets/images/gatsby-icon.png'
+        icon: "./src/assets/images/gatsby-icon.png",
       },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sitemap`,
-    'gatsby-plugin-robots-txt',
-    `gatsby-plugin-netlify`,
-    `gatsby-plugin-styled-components`
+    "gatsby-plugin-robots-txt",
+    `gatsby-plugin-styled-components`,
   ],
 }
