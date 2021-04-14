e636e48e666acfcc8b360fbc6ffc1687b9e769c4
Generosa Litton <glcodeworks@gmail.com>
2021-04-14 12:15:46 -0700

    added node setup for gatsby

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,38 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`)
+const { slash } = require(`gatsby-core-utils`)
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  // query content for WordPress posts
+  const {
+    data: {
+      allWpPage: { nodes: allPages },
+    },
+  } = await graphql(`
+    query {
+      allWpPage {
+        nodes {
+          id
+          uri
+        }
+      }
+    }
+  `)
+
+  const pageTemplate = path.resolve(`./src/templates/page.js`)
+
+  allPages.forEach((page) => {
+    createPage({
+      // will be the url for the page
+      path: page.uri,
+      // specify the component template of your choice
+      component: slash(pageTemplate),
+      // In the ^template's GraphQL query, 'id' will be available
+      // as a GraphQL variable to query for this post's data.
+      context: {
+        id: page.id,
+      },
+    })
+  })
+}
